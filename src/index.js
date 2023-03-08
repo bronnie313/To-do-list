ca438435c8890230017a7c476fea0c2681923136
Bisaso Ronald <bronnie313@gmail.com>
2023-03-08 09:50:45 +0300

    made content editable when you press enter key

--- src/index.js
+++ src/index.js
@@ -1,122 +1,77 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-// listName.sort((a, b) => a.index - b.index);
+const form = document.getElementById('form');
+const task = document.getElementById('task');
+const items = document.getElementById('items');
 
-const ul = document.getElementById('items');
-const listName = JSON.parse(window.localStorage.getItem('tasks')) || [];
+const listName = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 
-const populate = () => {
-  ul.innerHTML = '';
-  for (let i = 0; i < listName.length; i += 1) {
-    // const checkcompleted = listName[i].completed ? 'checked' : '';
-    const li = document.createElement('li');
-    li.className = 'to-do-item';
-    li.id = `${listName[i].index}`;
-    li.innerHTML = ` 
-            <input type="checkbox">
-            <p id="editable" class="editable">${listName[i].text}</p>
-            <button id="viewmore" class="view-more"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-            <button id="delete" class="deleteBtn"><i class="fa-solid fa-trash"></i></button>
-            <button id="edit" class="editBtn"><i class="fa-regular fa-pen-to-square"></i></button>
-            <button id="save" class="saveBtn"><i class="fa-regular fa-floppy-disk"></i></button>
-            
-            
-        `;
-    ul.appendChild(li);
-
-    const viewMore = document.querySelectorAll('.view-more');
-    const deleteBtn = document.querySelectorAll('.deleteBtn');
-    const editBtn = document.querySelectorAll('.editBtn');
-    const saveBtn = document.querySelectorAll('.saveBtn');
-
-    viewMore[i].addEventListener('click', () => {
-      viewMore[i].style.display = 'none';
-      deleteBtn[i].style.display = 'block';
-      editBtn[i].style.display = 'block';
-      saveBtn[i].style.display = 'none';
+// make task editable
+const makeTaskEditable = () => {
+  const taskAreas = document.querySelectorAll('.area');
+  taskAreas.forEach((taskArea, index) => {
+    taskArea.addEventListener('blur', () => {
+      listName[index].text = taskArea.innerText;
+      localStorage.setItem('tasks', JSON.stringify(listName));
     });
-
-    editBtn[i].addEventListener('click', () => {
-      saveBtn[i].style.display = 'block';
-      viewMore[i].style.display = 'none';
-      deleteBtn[i].style.display = 'none';
-      editBtn[i].style.display = 'none';
-    });
-
-    const editable = document.querySelectorAll('.editable');
-    const edit = document.querySelectorAll('.editBtn');
-
-    edit[i].addEventListener('click', (text) => {
-      editable[i].value = text;
-
-      editable[i].contentEditable = true;
-      editable[i].focus();
-    });
-
-    const editTask = li.querySelector('#editable');
-    const save = document.querySelectorAll('.saveBtn');
-
-    save[i].addEventListener('click', (event) => {
-      let todo3 = JSON.parse(window.localStorage.getItem('tasks'));
-      listName[i].text = editTask.textContent;
-      const c = event.target.parentNode;
-      const d = c.parentNode.parentNode.parentNode.id
-      console.log(textContent)
-      localStorage.setItem('task', JSON.stringify(todo3));
-      location.reload;
-      
-    });
-
-    save[i].addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        save[i].blur();
+    taskArea.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        listName[index].text = taskArea.innerText;
+        localStorage.setItem('tasks', JSON.stringify(listName));
+        taskArea.blur();
       }
     });
-
-    deleteBtn[i].addEventListener('click', (event) => {
-      let todo = JSON.parse(window.localStorage.getItem('tasks'));
-      const a = event.target.parentNode;
-      const b = a.parentNode.id;
-      const task = todo[b - 1];
-      todo = todo.filter((item) => item !== task);
-      localStorage.setItem('tasks', JSON.stringify(todo));
-
-      const todo2 = JSON.parse(window.localStorage.getItem('tasks'));
-      for (let j = 1; j <= todo2.length; j += 1) {
-        todo2[j - 1].index = j;
-      }
-      localStorage.setItem('tasks', JSON.stringify(todo2));
-      location.reload();
-    });
-
-    // save[i].addEventListener('blur', (text) => {
-    //   listName[i].text = textContent;
-    //   localStorage.setItem('tasks',JSON.stringify(listName));
-    //   location.reload
-    // });
-  }
+  });
 };
 
-populate();
+// display task
+const displayTask = () => {
+  items.innerHTML = '';
+  for (let i = 0; i < listName.length; i += 1) {
+    const isChecked = listName[i].completed ? 'checked' : '';
+    items.innerHTML += `
+        <div class="to-do-item">
+                <input id="check" type="checkbox" ${isChecked} onchange="checkcompleted(${i})">
+                <div id="area" class="area" contentEditable="true">${listName[i].text}</div>
+                <button id="delete" class="deleteBtn"><i onClick="deleteTask(${i})" class="fa-solid fa-trash-can"></i></button>
+        </div>
+        `;
+    task.value = '';
+  }
+  makeTaskEditable();
+};
 
-const enter = document.getElementById('enter');
-const addTask = document.getElementById('addTask');
-
-const createTask = (addTask) => {
-  const taskobject = {
+// adding task
+const addtask = (task) => {
+  const data = {
     completed: false,
-    text: addTask,
+    text: task,
     index: listName.length + 1,
   };
-  listName.push(taskobject);
+  listName.push(data);
   localStorage.setItem('tasks', JSON.stringify(listName));
 };
 
-enter.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  createTask(addTask.value);
-  addTask.value = '';
-  populate();
+  addtask(task.value);
+  displayTask();
 });
+
+// deleting task
+const deleteTask = (index) => {
+  listName.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(listName));
+  for (let i = 0; i < listName.length; i += 1) {
+    listName[i].index = i + 1;
+  }
+  localStorage.setItem('tasks', JSON.stringify(listName));
+  displayTask();
+};
+
+window.onload = () => {
+  displayTask();
+  deleteTask();
+};
