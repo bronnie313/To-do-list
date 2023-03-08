bea0e13907ff045bc3711142b3058ca51420a8cb
Bisaso Ronald <bronnie313@gmail.com>
2023-03-08 18:14:20 +0300

    fix errors on the delete button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-// import _ from 'lodash';
 import './style.css';
 
 const form = document.getElementById('form');
 const task = document.getElementById('task');
 const items = document.getElementById('items');
 
-const listName = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+let listName = [];
+
+const initList = () => {
+  listName = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+};
 
 // make task editable
 const makeTaskEditable = () => {
@@ -32,19 +35,19 @@ const displayTask = () => {
   for (let i = 0; i < listName.length; i += 1) {
     const isChecked = listName[i].completed ? 'checked' : '';
     items.innerHTML += `
-        <div class="to-do-item">
-                <input id="check" type="checkbox" ${isChecked} onchange="checkcompleted(${i})">
-                <div id="area" class="area" contentEditable="true">${listName[i].text}</div>
-                <button id="delete" class="deleteBtn"><i onClick="deleteTask(${i})" class="fa-solid fa-trash-can"></i></button>
-        </div>
-        `;
+      <div class="to-do-item">
+        <input id="check${i}" type="checkbox" ${isChecked}>
+        <div id="area${i}" class="area" contentEditable="true">${listName[i].text}</div>
+        <button id="delete${i}" class="deleteBtn"><i class="fa-solid fa-trash-can"></i></button>
+      </div>
+    `;
     task.value = '';
   }
   makeTaskEditable();
 };
 
 // adding task
-const addtask = (task) => {
+const addTask = (task) => {
   const data = {
     completed: false,
     text: task,
@@ -56,22 +59,26 @@ const addtask = (task) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addtask(task.value);
+  addTask(task.value);
   displayTask();
 });
 
 // deleting task
-const deleteTask = (index) => {
-  listName.splice(index, 1);
-  localStorage.setItem('tasks', JSON.stringify(listName));
-  for (let i = 0; i < listName.length; i += 1) {
-    listName[i].index = i + 1;
+items.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'i') {
+    const button = e.target.parentNode;
+    const index = parseInt(button.id.replace('delete', ''), 10);
+    listName.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(listName));
+    for (let i = 0; i < listName.length; i += 1) {
+      listName[i].index = i + 1;
+    }
+    localStorage.setItem('tasks', JSON.stringify(listName));
+    displayTask();
   }
-  localStorage.setItem('tasks', JSON.stringify(listName));
-  displayTask();
-};
+});
 
 window.onload = () => {
+  initList();
   displayTask();
-  deleteTask();
 };
